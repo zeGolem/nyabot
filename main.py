dbd84cb07d4ddfbfa6f3ef20ea3a18bdd24f22db
Sugaryy_ <you@example.com>
2023-02-24 19:56:51 +0100

    lines: Added custom line support

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ gifs = {}
 with open("gifs.json") as file:
     gifs = json.load(file)
 
-HELP_MESSAGE= """
+lines = {}
+with open("lines.json") as file:
+    lines = json.load(file)
+
+HELP_MESSAGE = """
 Commands:
 ```hug - Hugs target user  
 kiss - Kisses target user 
@@ -35,12 +39,13 @@ async def command_handler(message: discord.Message, command: list[str]):
     command_typed = command[0]
 
     target = message.author.name
+    sender = message.author.name
     if len(message.mentions) > 0:
         target = message.mentions[0].name
 
     if command_typed == "help":
         await message.reply(HELP_MESSAGE)
-        return        
+        return
 
     if command_typed not in gifs:
         await message.reply(
@@ -48,7 +53,10 @@ async def command_handler(message: discord.Message, command: list[str]):
         return
 
     choosen_gif = random.choice(gifs[command_typed])
-    await message.reply(f"here u go {target}\n{choosen_gif}")
+    choosen_line = random.choice(lines[command_typed])
+    choosen_line = choosen_line.replace("$1", sender)
+    choosen_line = choosen_line.replace("$2", target)
+    await message.reply(f"{choosen_line}\n{choosen_gif}")
 
 COMMANDS = [
     "hug",
